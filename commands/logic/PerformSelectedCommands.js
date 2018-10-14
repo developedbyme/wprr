ce64b6b0aea1fb04fce6fc8e44f64258525e1eee
Mattias Ekendahl <mattias@developedbyme.com>
2018-10-14 21:13:52 +0200

    Update: more usage of commands

---
+++ commands/logic/PerformSelectedCommands.js
@@ -0,0 +1,61 @@
+import Wprr from "wprr";
+
+import BaseCommand from "wprr/commands/BaseCommand";
+
+import CommandPerformer from "wprr/commands/CommandPerformer";
+
+//import PerformSelectedCommands from "wprr/commands/logic/PerformSelectedCommands";
+/**
+ * Command that performs a set of commands based on a selection.
+ */
+export default class PerformSelectedCommands extends BaseCommand {
+	
+	/**
+	 * Constructor
+	 */
+	constructor() {
+		super();
+		
+		this.setInput("selected", null);
+		this.setInput("options", new Array());
+	}
+	
+	perform() {
+		
+		let selected = this.getInput("selected");
+		let options = this.getInput("options");
+		
+		let isFound = false;
+		
+		let currentArray = options;
+		let currentArrayLength = currentArray.length;
+		for(let i = 0; i < currentArrayLength; i++) {
+			let currentOption = currentArray[i];
+			if(currentOption.key === selected) {
+				
+				CommandPerformer.perform(currentOption.value, this._eventData, this._triggerElement);
+				isFound = true;
+				break;
+			}
+		}
+		
+		if(!isFound) {
+			console.log("No commnads for key " + selected, this);
+		}
+	}
+	
+	addOption(aKey, aCommands) {
+		this.getRawInput("options").push({"key": aKey, "value": aCommands});
+		
+		return this;
+	}
+	
+	static create(aSelected = null, aOptions = null) {
+		let newPerformSelectedCommands = new PerformSelectedCommands();
+		
+		newPerformSelectedCommands.setInputWithoutNull("selected", aSelected);
+		newPerformSelectedCommands.setInputWithoutNull("options", aOptions);
+		
+		return newPerformSelectedCommands;
+	}
+}
--- elements/interaction/ClickOutsideTrigger.js
+++ elements/interaction/ClickOutsideTrigger.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 
 import ManipulationBaseObject from "wprr/manipulation/ManipulationBaseObject";
 
+import CommandPerformer from "wprr/commands/CommandPerformer";
+
 //import ClickOutsideTrigger from "wprr/elements/interaction/ClickOutsideTrigger";
 export default class ClickOutsideTrigger extends ManipulationBaseObject {
 
@@ -13,33 +15,42 @@ export default class ClickOutsideTrigger extends ManipulationBaseObject {
 	}
 	
 	_callback_click(aEvent) {
-		//console.log("wprr/elements/interaction/ClickOutsideTrigger::_callback_click");
+		console.log("wprr/elements/interaction/ClickOutsideTrigger::_callback_click");
 		
-		if(this.getSourcedProp("active")) {
-			let triggerName = this.getSourcedProp("triggerName");
+		if(this.getSourcedProp("active") !== false) {
 			
 			let currentElement = ReactDOM.findDOMNode(this);
-			
+		
 			if(!currentElement.contains(aEvent.srcElement)) {
-				let triggerController = this.getReference("trigger/" + triggerName);
-				if(triggerController) {
-					let shouldStopClick = this.getSourcedPropWithDefault("shouldStopClick", true);
-					if(shouldStopClick) {
-						aEvent.preventDefault();
-						aEvent.stopImmediatePropagation();
-					}
-					
-					triggerController.trigger(triggerName, null);
+				let commands = this.getSourcedProp("commands");
+				if(commands) {
+					let commandData = this.getSourcedProp("commandData");
+					CommandPerformer.perform(commands, commandData, this);
 				}
 				else {
-					console.warn("No controller for trigger " + triggerName, this);
+					//METODO: show upgrade notice
+					let triggerName = this.getSourcedProp("triggerName");
+				
+					let triggerController = this.getReference("trigger/" + triggerName);
+					if(triggerController) {
+						triggerController.trigger(triggerName, null);
+					}
+					else {
+						console.warn("No controller for trigger " + triggerName, this);
+					}
+				}
+			
+				let shouldStopClick = this.getSourcedPropWithDefault("shouldStopClick", true);
+				if(shouldStopClick) {
+					aEvent.preventDefault();
+					aEvent.stopImmediatePropagation();
 				}
 			}
 		}
 	}
 	
 	componentDidMount() {
-		//console.log("wprr/elements/interaction/ClickOutsideTrigger::componentDidMount");
+		console.log("wprr/elements/interaction/ClickOutsideTrigger::componentDidMount");
 		
 		document.body.addEventListener("click", this._callback_clickBound, true);
 	}
@@ -54,6 +65,8 @@ export default class ClickOutsideTrigger extends ManipulationBaseObject {
 		
 		let returnObject = super._manipulateProps(aReturnObject);
 		
+		delete returnObject["commands"];
+		delete returnObject["commandData"];
 		delete returnObject["triggerName"];
 		delete returnObject["triggerData"];
 		delete returnObject["active"];
