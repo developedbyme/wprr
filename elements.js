c4cb92cb37dc88340665f636f4f172bc1b6e05d3
Mattias Ekendahl <mattias@developedbyme.com>
2018-10-16 16:03:23 +0200

    Update: exporting all elements

---
+++ elements.js
@@ -0,0 +1,38 @@
+export {default as WprrBaseObject} from "wprr/WprrBaseObject";
+
+export {default as Link} from "wprr/elements/interaction/Link";
+export {default as CommandButton} from "wprr/elements/interaction/CommandButton";
+export {default as EventCommands} from "wprr/elements/interaction/EventCommands";
+export {default as ClickOutsideTrigger} from "wprr/elements/interaction/ClickOutsideTrigger";
+
+export {default as WprrDataLoader} from "wprr/manipulation/loader/WprrDataLoader";
+export {default as PartRenderFunction} from "wprr/elements/create/PartRenderFunction";
+export {default as ReferenceInjection} from "wprr/reference/ReferenceInjection";
+
+export {default as SourcedText} from "wprr/elements/text/SourcedText";
+
+export {default as RefGroup} from "wprr/reference/RefGroup";
+export {default as EditableProps} from "wprr/manipulation/EditableProps";
+export {default as FormField} from "wprr/elements/form/FormField";
+export {default as Selection} from "wprr/elements/form/Selection";
+export {default as Checkbox} from "wprr/elements/form/Checkbox";
+export {default as FlexRow} from "wprr/elements/area/grid/FlexRow";
+export {default as ValidatingForm} from "wprr/elements/form/ValidatingForm";
+export {default as ValidationBaseObject} from "wprr/elements/form/validation/ValidationBaseObject";
+export {default as OpenCloseExpandableArea} from "wprr/elements/area/OpenCloseExpandableArea";
+
+export {default as ElementSize} from "wprr/manipulation/measure/ElementSize";
+
+export {default as DeepDistribution} from "wprr/manipulation/distribution/DeepDistribution";
+export {default as DistributionTarget} from "wprr/manipulation/distribution/DistributionTarget";
+
+export {default as Markup} from "wprr/markup/Markup";
+export {default as MarkupChildren} from "wprr/markup/MarkupChildren";
+export {default as MarkupPlacement} from "wprr/markup/MarkupPlacement";
+export {default as UseMarkup} from "wprr/markup/UseMarkup";
+
+export {default as Loop} from "wprr/elements/create/Loop";
+
+export {default as WprrLazyImage} from "wprr/elements/image/WprrLazyImage";
+export {default as LazyImage} from "wprr/elements/image/LazyImage";
+export {default as WpMenu} from "wprr/wp/menu/WpMenu";
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import Wprr from "wprr/Wprr";
 
-console.log(">>>>>>>>>>>>>>");
-
 export default Wprr;
+
+import * as elements from "wprr/elements";
+
+for(let objectName in elements) {
+	Wprr.addAutonamedClasses(elements[objectName]);
+}
+
+export * from "wprr/elements";
