3e87ea37f2f914f7bc2e16edf4fccc93199d120e
Mattias Ekendahl <mattias@developedbyme.com>
2018-10-28 00:23:55 +0200

    Update: added links

--- injections/wp/InjectWpLoginForm.js
+++ injections/wp/InjectWpLoginForm.js
@@ -7,6 +7,12 @@ import SourceData from "wprr/reference/SourceData";
 import ReferenceInjection from "wprr/reference/ReferenceInjection";
 import EditableProps from "wprr/manipulation/EditableProps";
 import FormField from "wprr/elements/form/FormField";
+import SourceDataWithPath from "wprr/reference/SourceDataWithPath";
+import WprrDataLoader from "wprr/manipulation/loader/WprrDataLoader";
+import SourcedText from "wprr/elements/text/SourcedText";
+import Link from "wprr/elements/interaction/Link";
+import DeepDistribution from "wprr/manipulation/distribution/DeepDistribution";
+import DistributionTarget from "wprr/manipulation/distribution/DistributionTarget";
 
 //import InjectWpLoginForm from "wprr/injections/wp/InjectWpLoginForm";
 export default function InjectWpLoginForm(aProps) {
@@ -21,6 +27,30 @@ export default function InjectWpLoginForm(aProps) {
 		}
 	}
 	
+	let lostPasswordLink = React.createElement(WprrDataLoader, {"loadData": {"link": "wprr/v1/range-item/page/relation/default?relation=global-pages/lost-password"}},
+		React.createElement(DeepDistribution, {}, 
+			React.createElement(DistributionTarget, {}, 
+				React.createElement(Link, {"href": SourceDataWithPath.create("prop", "link", "permalink")},
+					React.createElement(DistributionTarget, {}, 
+						React.createElement(SourcedText, {"text": SourceDataWithPath.create("prop", "link", "title")})
+					)
+				)
+			)
+		)
+	);
+	
+	let signUpLink = React.createElement(WprrDataLoader, {"loadData": {"link": "wprr/v1/range-item/page/relation/default?relation=global-pages/sign-up"}},
+		React.createElement(DeepDistribution, {}, 
+			React.createElement(DistributionTarget, {}, 
+				React.createElement(Link, {"href": SourceDataWithPath.create("prop", "link", "permalink")},
+					React.createElement(DistributionTarget, {}, 
+						React.createElement(SourcedText, {"text": SourceDataWithPath.create("prop", "link", "title")})
+					)
+				)
+			)
+		)
+	);
+	
 	let injectData = {
 		"elements/login/usernameField": React.createElement(EditableProps, {"editableProps": "log", "log": ""},
 			React.createElement(FormField, {"valueName": "log", "name": "log", "type": "text"})
@@ -28,7 +58,9 @@ export default function InjectWpLoginForm(aProps) {
 		"elements/login/passwordField": React.createElement(EditableProps, {"editableProps": "pwd", "pwd": ""},
 			React.createElement(FormField, {"valueName": "pwd", "name": "pwd", "type": "password"})
 		),
-		"elements/login/submitButton": React.createElement("input", {"type": "submit", "value": "Submit"})
+		"elements/login/submitButton": React.createElement("input", {"type": "submit", "value": "Submit"}),
+		"elements/login/lostPasswordLink": lostPasswordLink,
+		"elements/login/signUpLink": signUpLink,
 	};
 	
 	return React.createElement(RefGroup, newProps,
