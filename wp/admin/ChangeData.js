81999585616bd4d6ad989523ebb4c11e15c9fda9
Mattias Ekendahl <mattias@developedbyme.com>
2018-12-06 11:15:27 +0100

    Added: change data

---
+++ wp/admin/ChangeData.js
@@ -0,0 +1,41 @@
+import ChangeDataFunctions from "wprr/wp/admin/ChangeDataFunctions";
+
+// import ChangeData from "wprr/wp/admin/ChangeData";
+export default class ChangeData  {
+	
+	constructor() {
+		this._title = "";
+		this._changes = new Array();
+	}
+	
+	getCreateData() {
+		return ChangeDataFunctions.createCreateData(this._title, this._changes);
+	}
+	
+	getEditData() {
+		return ChangeDataFunctions.currentChange(this._changes);
+	}
+	
+	addChange(aChange) {
+		this._changes.push(aChange);
+		
+		return this;
+	}
+	
+	setField(aField, aValue) {
+		
+		let currentChange = ChangeDataFunctions.createChangeDataWithSingleValue(aField, aValue);
+		
+		this.addChange(currentChange);
+		
+		return this;
+	}
+	
+	setTitle(aTitle) {
+		this._title = aTitle;
+		
+		this.setField("title", aTitle);
+		
+		return this;
+	}
+}
