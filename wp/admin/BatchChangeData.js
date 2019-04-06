d61d10d6ee81e48e3ea94cabdecbb7e00bfe931f
Mattias Ekendahl <mattias@developedbyme.com>
2019-04-06 02:01:20 +0200

    Update: added batch change data

---
+++ wp/admin/BatchChangeData.js
@@ -0,0 +1,33 @@
+import ChangeData from "wprr/wp/admin/ChangeData";
+
+// import BatchChangeData from "wprr/wp/admin/BatchChangeData";
+export default class BatchChangeData  {
+	
+	constructor() {
+		this._items = new Array();
+	}
+	
+	getBatchEditData() {
+		let changesArray = new Array();
+		
+		let currentArray = this._items;
+		let currentArrayLength = currentArray.length;
+		for(let i = 0; i < currentArrayLength; i++) {
+			let currentItem = currentArray[i];
+			
+			let newBatchChange = currentItem.changeData.getEditData();
+			newBatchChange["id"] = currentItem.id;
+			changesArray.push(newBatchChange);
+		}
+		
+		return {"batch": changesArray};
+	}
+	
+	createItemChange(aId) {
+		let newChangeData = new ChangeData();
+		
+		this._items.push({"id": aId, "changeData": newChangeData});
+		
+		return newChangeData;
+	}
+}
