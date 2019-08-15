dd5c41bbd20106d69ae295af518cda660e791185
Mattias Ekendahl <mattias@developedbyme.com>
2019-08-15 09:18:04 +0200

    Update: added add querystring util

--- utils/UrlFunctions.js
+++ utils/UrlFunctions.js
@@ -17,4 +17,28 @@ export default class UrlFunctions {
 		
 		return url;
 	}
+	
+	static addQueryString(aBaseUrl, aKey, aValue) {
+		
+		let url = aBaseUrl;
+		let hash = "";
+		
+		let hashIndex = aBaseUrl.indexOf("#");
+		if(hashIndex !== -1) {
+			hash = url.substring(hashIndex, url.length);
+			url = url.substring(0, hashIndex);
+		}
+		
+		let questionMarkIndex = url.indexOf("?");
+		if(questionMarkIndex !== -1) {
+			url += "&";
+		}
+		else {
+			url += "?";
+		}
+		
+		url += encodeURIComponent(aKey) + "=" + encodeURIComponent(aValue);
+		
+		return url + hash;
+	}
 }
