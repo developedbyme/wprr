6a84c9cfa8da8d2755b9d62d5505f4b95b331665
Mattias Ekendahl <mattias@developedbyme.com>
2020-08-12 12:12:20 +0200

    Update: using shared markup

--- elements/form/SteppedNavigation.js
+++ elements/form/SteppedNavigation.js
@@ -13,36 +13,26 @@ export default class SteppedNavigation extends WprrBaseObject {
 		
 		this._markup = null;
 		this._externalStorage = new Wprr.utils.DataStorage();
-		
-		this._callback_changeBound = this._callback_change.bind(this);
 	}
 	
-	_callback_change(aEvent) {
-		//console.log("wprr/elements/form/SteppedNavigation::_callback_change");
-		//console.log(aEvent);
-		//console.log(aEvent.target.value);
+	_goBack() {
+		console.log("wprr/elements/form/SteppedNavigation::_goBack");
 		
-		let newValue = aEvent.target.value;
-		
-		this.getReferences().getObject("value/" + this.props.valueName).updateValue(this.props.valueName, newValue);
-		
-		let commands = this.getSourcedProp("changeCommands");
+		let commands = this.getSourcedProp("backCommands");
 		
 		if(commands) {
 			CommandPerformer.perform(commands, newValue, this);
 		}
 	}
 	
-	_goBack() {
-		console.log("wprr/elements/form/SteppedNavigation::_goBack");
-		
-		//METODO
-	}
-	
 	_goForward() {
 		console.log("wprr/elements/form/SteppedNavigation::_goForward");
 		
-		//METODO
+		let commands = this.getSourcedProp("commands");
+		
+		if(commands) {
+			CommandPerformer.perform(commands, newValue, this);
+		}
 	}
 	
 	_prepareRender() {
@@ -79,86 +69,76 @@ export default class SteppedNavigation extends WprrBaseObject {
 	_renderMainElement() {
 		//console.log("wprr/elements/form/SteppedNavigation::_renderMainElement");
 		
-		let activeButton = <Wprr.CommandButton commands={Wprr.commands.callFunction(Wprr.sourceReference("steppedNavigation"), "_goForward", [])}>
-			<div className="icon-button-round large">
-				<Wprr.Image src="arrow-right-white.svg" className="background-contain icon-button-round-image" />
-			</div>
-		</Wprr.CommandButton>;
-		
-		let inactiveButton = <div className="icon-button-round large disabled">
-			<Wprr.Image src="arrow-right-white.svg" className="background-contain icon-button-round-image" />
-		</div>;
-		
-		let activeBackButton = <Wprr.CommandButton commands={Wprr.commands.callFunction(Wprr.sourceReference("steppedNavigation"), "_goBack", [])}>
-			<div className="icon-button-round outlined">
-				<Wprr.Image src="arrow-left-standard.svg" className="background-contain icon-button-round-image" />
-			</div>
-		</Wprr.CommandButton>;
-		
-		let inactiveBackButton = <div className="icon-button-round outlined disabled">
-			<Wprr.Image src="arrow-left-standard.svg" className="background-contain icon-button-round-image" />
-		</div>;
-		
-		let layout = React.createElement(Wprr.FlexRow, {className: "justify-between small-item-spacing vertically-center-items", itemClasses: "width-50,flex-no-resize,width-50"},
-			React.createElement(Wprr.FlexRow, {className: "justify-between"},
-				React.createElement("div"),
-				React.createElement(Wprr.ExternalStorageProps, {"props": "backState", "externalStorage": Wprr.sourceReference("steppedNavigation/externalStorage")},
-					React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(
-						Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", Wprr.sourceCombine("backButton.", Wprr.sourceProp("backState"))),
-						Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "backButton.default")
-					)})
-				)
-			),
-			React.createElement(Wprr.ExternalStorageProps, {"props": "state", "externalStorage": Wprr.sourceReference("steppedNavigation/externalStorage")},
-				React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(
-					Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", Wprr.sourceCombine("button.", Wprr.sourceProp("state"))),
-					Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "button.default")
-				)})
-			),
-			React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(
-				Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "loop")
-			)})
-		);
-		
-		let defaultElements = {
-			"button": {
-				"active": activeButton,
-				"inactive": inactiveButton,
-				"default": activeButton
-			},
-			"backButton": {
-				"active": activeBackButton,
-				"inactive": inactiveBackButton,
-				"default": inactiveBackButton
-			},
-			"layout": layout,
-			"loop": React.createElement(Wprr.ExternalStorageProps, {"props": "currentStep,numberOfSteps", "externalStorage": Wprr.sourceReference("steppedNavigation/externalStorage")},
-				Wprr.Loop.createMarkupLoop(Wprr.utils.array.createRange(Wprr.sourceProp("currentStep"), Wprr.sourceProp("numberOfSteps")), <div className="pager-circle" />, null, <Wprr.FlexRow className="micro-item-spacing" />)
-			)
-		};
-		
-		let injectData = {
-			"steppedNavigation/defaultElements": defaultElements
-		};
-		
-		let staticMarkup = React.createElement(Wprr.ReferenceInjection, {"injectData": injectData}, 
-			React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "layout"))})
-		);
-		
-		return React.createElement("div", {"className": "stepped-navigation-holder"},
-			React.createElement(Wprr.ReferenceInjection,
-				{"injectData": {
-					"steppedNavigation": this,
-					"steppedNavigation/externalStorage": this._externalStorage
-				}}, 
-				staticMarkup
-			)
-		);
+		return this._getMarkup();
 	}
 	
 	static getSharedMarkup() {
 		if(!SteppedNavigation._SHARED_MARKUP) {
-			
+			let activeButton = <Wprr.CommandButton commands={Wprr.commands.callFunction(Wprr.sourceReference("steppedNavigation"), "_goForward", [])}>
+				<div className="icon-button-round large">
+					<Wprr.Image src="arrow-right-white.svg" className="background-contain icon-button-round-image" />
+				</div>
+			</Wprr.CommandButton>;
+		
+			let inactiveButton = <div className="icon-button-round large disabled">
+				<Wprr.Image src="arrow-right-white.svg" className="background-contain icon-button-round-image" />
+			</div>;
+		
+			let activeBackButton = <Wprr.CommandButton commands={Wprr.commands.callFunction(Wprr.sourceReference("steppedNavigation"), "_goBack", [])}>
+				<div className="icon-button-round outlined">
+					<Wprr.Image src="arrow-left-standard.svg" className="background-contain icon-button-round-image" />
+				</div>
+			</Wprr.CommandButton>;
+		
+			let inactiveBackButton = <div className="icon-button-round outlined disabled">
+				<Wprr.Image src="arrow-left-standard.svg" className="background-contain icon-button-round-image" />
+			</div>;
+		
+			let layout = React.createElement(Wprr.FlexRow, {className: "justify-between small-item-spacing vertically-center-items", itemClasses: "width-50,flex-no-resize,width-50"},
+				React.createElement(Wprr.FlexRow, {className: "justify-between"},
+					React.createElement("div"),
+					React.createElement(Wprr.ExternalStorageProps, {"props": "backState", "externalStorage": Wprr.sourceReference("steppedNavigation/externalStorage")},
+						React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(
+							Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", Wprr.sourceCombine("backButton.", Wprr.sourceProp("backState"))),
+							Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "backButton.default")
+						)})
+					)
+				),
+				React.createElement(Wprr.ExternalStorageProps, {"props": "state", "externalStorage": Wprr.sourceReference("steppedNavigation/externalStorage")},
+					React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(
+						Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", Wprr.sourceCombine("button.", Wprr.sourceProp("state"))),
+						Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "button.default")
+					)})
+				),
+				React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(
+					Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "loop")
+				)})
+			);
+		
+			let defaultElements = {
+				"button": {
+					"active": activeButton,
+					"inactive": inactiveButton,
+					"default": activeButton
+				},
+				"backButton": {
+					"active": activeBackButton,
+					"inactive": inactiveBackButton,
+					"default": inactiveBackButton
+				},
+				"layout": layout,
+				"loop": React.createElement(Wprr.ExternalStorageProps, {"props": "currentStep,numberOfSteps", "externalStorage": Wprr.sourceReference("steppedNavigation/externalStorage")},
+					Wprr.Loop.createMarkupLoop(Wprr.utils.array.createRange(Wprr.sourceProp("currentStep"), Wprr.sourceProp("numberOfSteps")), <div className="pager-circle" />, null, <Wprr.FlexRow className="micro-item-spacing" />)
+				)
+			};
+		
+			let injectData = {
+				"steppedNavigation/defaultElements": defaultElements
+			};
+		
+			SteppedNavigation._SHARED_MARKUP = React.createElement(Wprr.ReferenceInjection, {"injectData": injectData}, 
+				React.createElement(Wprr.InsertElement, {"element": Wprr.sourceFirst(Wprr.sourceReferenceIfExists("steppedNavigation/defaultElements", "layout"))})
+			);
 		}
 		
 		return SteppedNavigation._SHARED_MARKUP;
