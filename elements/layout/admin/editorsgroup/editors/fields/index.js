7c2edbfc6a80afe5ee456c0d379b0b2989df059e
Mattias Ekendahl <mattias@developedbyme.com>
2024-07-12 11:11:12 +0200

    Update: added string field

--- elements/layout/admin/editorsgroup/editors/fields/index.js
+++ elements/layout/admin/editorsgroup/editors/fields/index.js
@@ -1,3 +1,4 @@
 export {default as Name} from "./Name";
 export {default as Address} from "./Address";
 export {default as SingleRelation} from "./SingleRelation";
+export {default as String} from "./String";
